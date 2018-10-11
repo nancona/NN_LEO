4ef8fc4eb90df5247ac4dd7b0f76cba54b2a9677
nancona <n.ancona93@gmail.com>
2018-10-11 17:59:21 +0200

    nn 83% accuracy ==> in progress

--- NN.py
+++ NN.py
@@ -8,34 +8,92 @@ import pandas as pd
 input_file = pd.read_csv("rbdl_leo2606_Animation-learn-0.csv")
 input_file = input_file.values
 
-training_epochs = 5000
-display_step = 1000
-batch_size = 64
+training_epochs = 10000
+display_step = 100
+batch_size = 128
 input_dim = 24
 output_dim = 18
 train_size = 0.8
 
-# TO DO: define train input e test input
-train_input = 0
+sample_size = 10000
 
-def net (x, weights, biases, keep_prob):
+# Sample vector initialization
+position_train = []
+velocity_train = []
+action_train = []
+next_position_train = []
+next_velocity_train = []
 
-    layer_1 = tf.add(tf.matmul(x, weights['h1'], biases['b1']))
+position_test = []
+velocity_test = []
+action_test = []
+next_position_test = []
+next_velocity_test = []
+
+# neural network parameters
+n_hidden_1 = 200
+n_hidden_2 = 200
+learning_rate = 0.0001
+keep_prob = 0.8 # dropout probability
+
+# Defining train input/output e test input/output
+for i in range(sample_size):
+    if i == 0:
+        position_train = input_file[i][1:10]
+        velocity_train = input_file[i][10:19]
+        action_train = input_file[i][50:56]
+        next_position_train = input_file[i][29:38]
+        next_velocity_train = input_file[i][38:47]
+
+    if i > 0 and i < 9000:
+        position_train = np.vstack([position_train, input_file[i][1:10]])
+        velocity_train = np.vstack([velocity_train, input_file[i][10:19]])
+        action_train = np.vstack([action_train, input_file[i][50:56]])
+        next_position_train = np.vstack([next_position_train, input_file[i][29:38]])
+        next_velocity_train = np.vstack([next_velocity_train, input_file[i][38:47]])
+
+    if i == 9000:
+        position_test = input_file[i][1:10]
+        velocity_test = input_file[i][10:19]
+        action_test = input_file[i][50:56]
+        next_position_test = input_file[i][29:38]
+        next_velocity_test = input_file[i][38:47]
+
+    if i > 9000:
+        position_test = np.vstack([position_test, input_file[i][1:10]])
+        velocity_test = np.vstack([velocity_test, input_file[i][10:19]])
+        action_test = np.vstack([action_test, input_file[i][50:56]])
+        next_position_test = np.vstack([next_position_test, input_file[i][29:38]])
+        next_velocity_test = np.vstack([next_velocity_test, input_file[i][38:47]])
+
+# Train samples vector
+train_input = np.hstack([position_train, velocity_train, action_train])
+train_output = np.hstack([next_position_train, next_velocity_train])
+# Test samples vector
+test_input = np.hstack([position_test, velocity_test, action_test])
+test_output = np.hstack([next_position_test, next_velocity_test])
+
+
+def net(x, weights, biases, keep_prob):
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
     layer_1 = tf.nn.dropout(layer_1, keep_prob)
-    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    layer_2 = tf.nn.dropout(layer_2, keep_prob)
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
     return out_layer
-n_hidden_1 = 300
 
-n_input = train
 
 weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'out': tf.Variable(tf.random_normal([n_hidden_1, output_dim]))
+    'h1': tf.Variable(tf.random_normal([input_dim, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, output_dim]))
 }
 
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
     'out': tf.Variable(tf.random_normal([output_dim]))
 }
 
@@ -46,23 +104,23 @@ output = tf.placeholder("float", [None, output_dim])
 nn = net(x=input, weights=weights, biases=biases, keep_prob=keep_prob)
 
 cost = tf.losses.mean_squared_error(labels=output, predictions=nn)
-optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for epoch in range(training_epochs):
         avg_cost = 0.0
-        total_batch = int(len(input_train/batch_size))
-        input_train_batches = np.array_split(input_train, total_batch)
-        output_train_batches = np.array_split(output_train, total_batch)
+        total_batch = int(len(train_input) / batch_size)
+        train_input_batches = np.array_split(train_input, total_batch)
+        train_output_batches = np.array_split(train_output, total_batch)
 
         for i in range(total_batch):
-            batch_it, batch_ot = input_train_batches[i], output_train_batches[i]
+            batch_it, batch_ot = train_input_batches[i], train_output_batches[i]
             _, c = sess.run([optimizer, cost],
                             feed_dict={
-                                input:batch_it,
-                                output:batch_ot,
+                                input: batch_it,
+                                output: batch_ot,
                                 keep_prob:1})
             avg_cost += c / total_batch
         if epoch % display_step == 0:
@@ -72,4 +130,4 @@ with tf.Session() as sess:
 
     correct_prediction = tf.equal(tf.argmax(nn, 1), tf.argmax(output, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print("Accuracy:", accuracy.eval({input: input_test, output: output_test, keep_prob: 1.0}))
+    print("Accuracy:", accuracy.eval({input: test_input, output: test_output, keep_prob: 1.0}))
