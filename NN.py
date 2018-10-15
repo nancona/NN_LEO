1f491b6bac471fb93f9f54e16539974f30546bb7
nancona <n.ancona93@gmail.com>
2018-10-15 17:57:34 +0200

    progress

--- NN.py
+++ NN.py
@@ -4,16 +4,33 @@ import math
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtk3agg import FigureCanvas
+from matplotlib.backends.backend_gtk3 import (
+    NavigationToolbar2GTK3 as NavigationToolbar)
+
+win = Gtk.Window()
+win.connect("destroy", lambda x: Gtk.main_quit())
+win.set_default_size(400, 300)
+win.set_title("Embedding in GTK")
+
+vbox = Gtk.VBox()
+win.add(vbox)
+
 # importing datasets from simulation
 input_file = pd.read_csv("rbdl_leo2606_Animation-learn-0.csv")
 input_file = input_file.values
 
-training_epochs = 100
-display_step = 1000
+training_epochs = 30000
+display_step = 500
 batch_size = 128
 input_dim = 24
 output_dim = 18
-train_size = 0.8
+train_size = 0.8  # useless at the moment
 
 sample_size = 10000
 
@@ -31,10 +48,10 @@ next_position_test = []
 next_velocity_test = []
 
 # neural network parameters
-n_hidden_1 = 40
-n_hidden_2 = 100
+n_hidden_1 = 100
+n_hidden_2 = 50
 learning_rate = 0.001
-keep_prob = 1.0  # dropout probability
+Keep_prob = 0.8  # dropout probability
 
 # save and restore ops
 
@@ -76,16 +93,35 @@ train_output = np.hstack([next_position_train, next_velocity_train])
 test_input = np.hstack([position_test, velocity_test, action_test])
 test_output = np.hstack([next_position_test, next_velocity_test])
 
+
 # Comment/uncomment in order to define the multilayer net (1. one layer, 2. two layer)
 # still in design --- smaller networks seem to perform better
-def net(x, weights, biases, keep_prob):
+def net(x, keep_prob):
+    weights = {
+        'h1': tf.Variable(tf.random_normal([input_dim, n_hidden_1])),
+        'out': tf.Variable(tf.random_normal([n_hidden_1, output_dim]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'out': tf.Variable(tf.random_normal([output_dim]))
+    }
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
     layer_1 = tf.nn.dropout(layer_1, keep_prob)
     out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
     return out_layer
 
-# def net(x, weights, biases, keep_prob):
+# def net(x, keep_prob):
+#     weights = {
+#         'h1': tf.Variable(tf.random_normal([input_dim, n_hidden_1])),
+#         'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+#         'out': tf.Variable(tf.random_normal([n_hidden_2, output_dim]))
+#     }
+#     biases = {
+#         'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+#         'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+#         'out': tf.Variable(tf.random_normal([output_dim]))
+#     }
 #     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
 #     layer_1 = tf.nn.relu(layer_1)
 #     layer_1 = tf.nn.dropout(layer_1, keep_prob)
@@ -96,23 +132,24 @@ def net(x, weights, biases, keep_prob):
 #     return out_layer
 
 
-weights = {
-    'h1': tf.Variable(tf.random_normal([input_dim, n_hidden_1])),
-    # 'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_1, output_dim]))
-}
-
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    # 'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([output_dim]))
-}
+# weights = {
+#     'h1': tf.Variable(tf.random_normal([input_dim, n_hidden_1])),
+#     # 'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+#     'out': tf.Variable(tf.random_normal([n_hidden_1, output_dim]))
+# }
+#
+# biases = {
+#     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+#     # 'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+#     'out': tf.Variable(tf.random_normal([output_dim]))
+# }
 
 keep_prob = tf.placeholder("float")
 input = tf.placeholder("float", [None, input_dim])
 output = tf.placeholder("float", [None, output_dim])
 
-nn = net(x=input, weights=weights, biases=biases, keep_prob=keep_prob)
+nn = net(x=input, keep_prob=keep_prob)
+# nn = net(x=input, weights=weights, biases=biases, keep_prob=keep_prob)
 
 cost = tf.losses.mean_squared_error(labels=output, predictions=nn)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
@@ -132,7 +169,7 @@ with tf.Session() as sess:
                             feed_dict={
                                 input: batch_it,
                                 output: batch_ot,
-                                keep_prob:1})
+                                keep_prob: Keep_prob})
             avg_cost += c / total_batch
         if epoch % display_step == 0:
             print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
@@ -142,13 +179,42 @@ with tf.Session() as sess:
     # saving model
     tf.train.Saver().save(sess, "./model.ckpt")
 
-with tf.Session() as sess:
-    ckpt = tf.train.get_checkpoint_state('./model')
-    tf.train.Saver().restore(sess, ckpt.model_checkpoint_path)
-    #feed_dict = {input: test_input}
-    predictions = sess.run([test_input])
+    test_input_pred = tf.convert_to_tensor(test_input)
+    prediction = nn.eval({input: test_input, keep_prob: 1})
+    print prediction
+    print test_output
 
-    # correct_prediction = tf.equal(tf.argmax(nn, 1), tf.argmax(output, 1))
+    # plot predictions
+    time_sample = 0.03
+    sample_plot = np.arange(0, 100, 1)
+    time = sample_plot * time_sample
+
+    fig = Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(111)
+    ax.plot(time, test_output[0:100, 2], 'g', time, prediction[0:100, 2], 'b--')
+
+    canvas = FigureCanvas(fig)  # a Gtk.DrawingArea
+    vbox.pack_start(canvas, True, True, 0)
+    toolbar = NavigationToolbar(canvas, win)
+    vbox.pack_start(toolbar, False, False, 0)
+
+    win.show_all()
+    Gtk.main()
+    # plt.plot(time, test_output[:,2],'g', time, prediction[:,2], 'b--')
+    # plt.show()
+
     # # print sess.run(nn, feed_dict={input: test_input})
-    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    # print("Accuracy:", accuracy.eval({input: test_input, output: test_output, keep_prob: 1.0}))
+
+    # evaluation of the model (still not clear)
+    correct_prediction = tf.equal(tf.argmax(nn, 1), tf.argmax(output, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({input: test_input, output: test_output, keep_prob: 1.0}))
+
+
+# with tf.Session() as sess:
+#     ckpt = tf.train.get_checkpoint_state('./model')
+#     tf.train.Saver().restore(sess, ckpt.model_checkpoint)
+#     #feed_dict = {input: test_input}
+#     predictions = sess.run([test_input])
+
+
